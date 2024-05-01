1916a2eff3ed32a3b62041ddb17dd22c5e1caaa3
José Alberto Matioli <j.a.matioli@gmail.com>
2024-04-30 21:16:50 -0300

    Acrescentada classe ContasReceber, ContaReceberRepositpry e ContaReceberService.

---
+++ Dados/Client.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dados
+{
+    class Client
+    {
+        public int Id { get; set; }
+        public string Cpf_Cnpj { get; set; }
+        public string RazaoSocial { get; set; }
+
+        public Client()
+        {
+        }
+
+        public Client(int id, string cpf_Cnpj, string razaoSocial)
+        {
+            Id = id;
+            Cpf_Cnpj = cpf_Cnpj;
+            RazaoSocial = razaoSocial;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Client client &&
+                   Id == client.Id &&
+                   Cpf_Cnpj == client.Cpf_Cnpj &&
+                   RazaoSocial == client.RazaoSocial;
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = -815749117;
+            hashCode = hashCode * -1521134295 + Id.GetHashCode();
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Cpf_Cnpj);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(RazaoSocial);
+            return hashCode;
+        }
+    }
+}
---
+++ Dados/ContaReceber.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dados
+{
+    public class ContaReceber
+    {
+        public int Id { get; set; }
+        public decimal Valor { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public DateTime DataVencimento { get; set; }
+        public DateTime DataRecebimento { get; set; }
+        public decimal ValorRecebido { get; set; }
+        public MeioDePagamento MeioPagamento { get; set; }
+        public EstadoPagamento EstadoPagamento { get; set; }
+
+        public ContaReceber()
+        {
+        }
+
+        public ContaReceber(int id, decimal valor, DateTime dataCadastro, DateTime dataVencimento, MeioDePagamento meioPagamento, EstadoPagamento estadoPagamento)
+        {
+            Id = id;
+            Valor = valor;
+            DataCadastro = dataCadastro;
+            DataVencimento = dataVencimento;
+            MeioPagamento = meioPagamento;
+            EstadoPagamento = estadoPagamento;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is ContaReceber receber &&
+                   Id == receber.Id &&
+                   Valor == receber.Valor &&
+                   DataCadastro == receber.DataCadastro &&
+                   DataVencimento == receber.DataVencimento &&
+                   DataRecebimento == receber.DataRecebimento &&
+                   ValorRecebido == receber.ValorRecebido &&
+                   MeioPagamento == receber.MeioPagamento &&
+                   EstadoPagamento == receber.EstadoPagamento;
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = -626096533;
+            hashCode = hashCode * -1521134295 + Id.GetHashCode();
+            hashCode = hashCode * -1521134295 + Valor.GetHashCode();
+            hashCode = hashCode * -1521134295 + DataCadastro.GetHashCode();
+            hashCode = hashCode * -1521134295 + DataVencimento.GetHashCode();
+            hashCode = hashCode * -1521134295 + DataRecebimento.GetHashCode();
+            hashCode = hashCode * -1521134295 + ValorRecebido.GetHashCode();
+            hashCode = hashCode * -1521134295 + MeioPagamento.GetHashCode();
+            hashCode = hashCode * -1521134295 + EstadoPagamento.GetHashCode();
+            return hashCode;
+        }
+    }
+}
